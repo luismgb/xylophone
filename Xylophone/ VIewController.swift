0112528a946af3557719f07d7cf7cf68acf60f25
luismgb <luismigelgb@gmail.com>
2017-12-16 09:18:47 -0800

    Switch to AudioToolbox for audio playback

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,29 +7,24 @@
 //
 
 import UIKit
-import AVFoundation
+import AudioToolbox
 
 class ViewController: UIViewController{
     
-    var player: AVAudioPlayer?
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        let url = Bundle.main.url(forResource: "note\(sender.tag)", withExtension: "wav")!
-        
-        do {
-            player = try AVAudioPlayer(contentsOf: url)
-            guard let player = player else { return }
-            
-            player.prepareToPlay()
-            player.play()
-        } catch {
-            print(error.localizedDescription)
+        if let soundURL = Bundle.main.url(forResource: "note\(sender.tag)", withExtension: "wav") {
+            var mySound: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+            // Play
+            AudioServicesPlaySystemSound(mySound)
         }
+        
+        
     }
     
 }
